c0568cf2877b7e34a073923c37af9556c1f3e7ae
Maks <81374815+vermax2016@users.noreply.github.com>
2024-02-19 08:51:52 +0300

    Update compare_lexico.cpp

--- compare_lexico.cpp
+++ compare_lexico.cpp
@@ -8,26 +8,20 @@ using namespace std;
 bool comp(char s1, char s2) {
     return tolower(s1) < tolower(s2);
 }
-
 int main() {
     int n;
     cin >> n;
     vector<string> words;
-    for(int i = 0; i < n; ++i)
-    {
+    for(int i = 0; i < n; ++i) {
         string word;
         cin >> word;
-        
         words.push_back(word);
     }
-    
     sort(words.begin(), words.end(), [](string& Word1, string& word2)
-    {
-        return lexicographical_compare(Word1.begin(), Word1.end(), word2.begin(), word2.end(), comp);
-    });
-
-    for(string word : words)
-    {
+        {
+            return lexicographical_compare(Word1.begin(), Word1.end(), word2.begin(), word2.end(), comp);
+        });
+    for(string word : words) {
         cout << word << " ";
     }
     cout << "\n";
