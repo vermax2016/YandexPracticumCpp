1a317f20968f3a9f4b15a4b2b57272aca59fa9cf
vermax2016 <vermax2016@gmail.com>
2024-03-03 16:14:45 +0300

    Сортировка пар в компараторе с учетом статусов документов

--- sort_rating_relevance_status.cpp
+++ sort_rating_relevance_status.cpp
@@ -4,8 +4,15 @@
 
 using namespace std;
 
+enum class Status {
+    ACTUAL,
+    EXPIRED,
+    DELETED
+};
+
 struct Document {
     int id;
+    Status status;
     double relevance;
     int rating;
 };
@@ -13,19 +20,23 @@ struct Document {
 void SortDocuments(vector<Document>& matched_documents) {            
     sort(matched_documents.begin(), matched_documents.end(),
          [](const Document& lhs, const Document& rhs) {
-             return pair(rhs.rating, rhs.relevance) < pair(lhs.rating, lhs.relevance);
+             return lhs.status < rhs.status || (lhs.status == rhs.status && pair(lhs.rating, lhs.relevance)
+                  > pair(rhs.rating, rhs.relevance));
          });
 }
 
 int main() {
     vector<Document> documents = {
-        {100, 0.5, 4},
-        {101, 1.2, 4},
-        {102, 0.3, 5}
+        {100, Status::ACTUAL,  0.5, 4},
+        {101, Status::EXPIRED, 0.5, 4},
+        {102, Status::ACTUAL,  1.2, 4},
+        {103, Status::DELETED, 1.2, 4},
+        {104, Status::ACTUAL,  0.3, 5},
     };
     SortDocuments(documents);
     for (const Document& document : documents) {
         cout << document.id << ' '
+             << static_cast<int>(document.status) << ' '
              << document.relevance << ' '
              << document.rating << endl;
     }
